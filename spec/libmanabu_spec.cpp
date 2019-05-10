e1a792017d2ffd27eabf8c56280b12b7b3048c11
Rika Yoshida <rika.yoshida@nnh.go.jp>
2019-05-10 10:58:15 +0900

    add student spec

--- spec/libmanabu_spec.cpp
+++ spec/libmanabu_spec.cpp
@@ -43,15 +43,27 @@ BOOST_AUTO_TEST_SUITE(connectivity_suite)
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(student_suite)
-	// Check that the [test] server is running
+	// Check that the student
 	BOOST_AUTO_TEST_CASE(student_save_update_delete) {
 		Manabu::Manabu manabu = Manabu::Manabu();
 		manabu.connect("http", "localhost", 9000);
 		manabu.authenticate("admin", "123456");
-		Manabu::Student student = Manabu::Student(manabu.transactor, "エンジン", "学", 1999, 9, 1);
+
+		Manabu::Student student = Manabu::Student(manabu.transactor);
+		BOOST_CHECK_EQUAL(student.gender, Manabu::UNKNOWN); // Initial state
+
+		student = Manabu::Student(manabu.transactor, "エンジン", "学", 1999, 9, 1);
 		BOOST_CHECK_EQUAL(student.save(), true); // Initial save
+		BOOST_CHECK_EQUAL(student.surname, "エンジン");
+
+		student.name = "";
+		BOOST_CHECK_EQUAL(student.save(), false); // Validation error
+
+		student.name = "学";
 		student.middleName = "Flux";
 		BOOST_CHECK_EQUAL(student.save(true), true); // Update
+		BOOST_CHECK_EQUAL(student.middleName, "Flux");
+
 		BOOST_CHECK_EQUAL(student.remove(), true); // Remove
 	}
 BOOST_AUTO_TEST_SUITE_END()
